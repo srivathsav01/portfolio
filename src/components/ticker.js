b74439afd1d1ee0415339983bf6cfa6b2a68683c
srivathsav01 <srivathsav.m.2001@gmail.com>
2025-01-25 17:54:58 +0530

    Resolved Warnings

--- src/components/ticker.js
+++ src/components/ticker.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const Ticker = ({ items, speed = 30 }) => {
   const alphabet = `0123456789`;
@@ -10,46 +10,47 @@ const Ticker = ({ items, speed = 30 }) => {
     return arr;
   };
 
-  const startTicker = (prev, next) => {
-    const prevArr = fillArray(prev.split(""), next.length);
-    const nextArr = fillArray(next.split(""), prev.length);
+  const startTicker = useCallback((prev, next) => {
+      const prevArr = fillArray(prev.split(""), next.length);
+      const nextArr = fillArray(next.split(""), prev.length);
 
-    let intervalIds = [];
-    let tempDisplay = [...prevArr];
+      let intervalIds = [];
+      let tempDisplay = [...prevArr];
 
-    nextArr.forEach((char, i) => {
-      if (char === prevArr[i]) return;
+      nextArr.forEach((char, i) => {
+        if (char === prevArr[i]) return;
 
-      let index = alphabet.indexOf(prevArr[i]);
-      const intervalId = setInterval(() => {
-        index = index === alphabet.length - 1 ? 0 : index + 1;
-        tempDisplay[i] = alphabet[index];
-        setDisplayText(tempDisplay.join(""));
+        let index = alphabet.indexOf(prevArr[i]);
+        const intervalId = setInterval(() => {
+          index = index === alphabet.length - 1 ? 0 : index + 1;
+          tempDisplay[i] = alphabet[index];
+          setDisplayText(tempDisplay.join(""));
 
-        if (tempDisplay[i] === char) {
-          clearInterval(intervalId);
-        }
-      }, speed);
+          if (tempDisplay[i] === char) {
+            clearInterval(intervalId);
+          }
+        }, speed);
 
-      intervalIds.push(intervalId);
-    });
+        intervalIds.push(intervalId);
+      });
 
-    return () => {
-      intervalIds.forEach((id) => clearInterval(id));
-    };
-  };
+      return () => {
+        intervalIds.forEach((id) => clearInterval(id));
+      };
+    },[alphabet,speed]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const nextIndex = (currentIndex + 1) % items.length;
       const prevText = displayText;
       const nextText = items[nextIndex];
-      startTicker(prevText, nextText);
+      const cleanup = startTicker(prevText, nextText);
       setCurrentIndex(nextIndex);
+      return cleanup;
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [currentIndex, displayText, items]);
+  }, [currentIndex, displayText, items,startTicker]);
 
   return (
     <div id="ticker" style={{ cursor: "pointer" }}>
